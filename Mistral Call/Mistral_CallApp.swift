1ce4a5318738b78208d867f22514dc629898f2ac
Marvin Kaunda <marvinkr@icloud.com>
2024-05-25 18:46:01 +0200

    Initial Commit

---
+++ Mistral Call/Mistral_CallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mistral_CallApp.swift
+//  Mistral Call
+//
+//  Created by Marvin Kaunda on 25/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Mistral_CallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
